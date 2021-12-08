209c5fab3ded80db03a02685f30657b652cc0891
briannguyen4 <brian4nguyen@gmail.com>
2021-12-08 15:15:54 -0500

    work on routes

--- index.js
+++ index.js
@@ -2,17 +2,25 @@ const express = require("express");
 const app = express();
 const mongoose = require("mongoose");
 const dotenv = require("dotenv");
+const userRoute = require("./routes/user");
+
 
 dotenv.config();
+
+app.listen(process.env.PORT || 5000, () => {
+    console.log("Background server is running!")
+});
 
 mongoose.connect(
     process.env.MONGO_URL)
     .then(() => console.log("DB connection successful"))
     .catch((err) => console.log(err));
 
-app.get("/api/test", () => console.log("Test is sucessful"));
+// app.get('/', (req, res) => {
+//     res.send('hi');
+// })
 
-app.listen(process.env.PORT || 5000, () => {
-    console.log("Background server is running!")
-});
+// app.get("/api/test", () => console.log("Test is sucessful"));
+
+app.use('/api/user', userRoute);
 
