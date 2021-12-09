ecacb2ad97271b9232ec74b0f9f217345eab588b
briannguyen4 <brian4nguyen@gmail.com>
2021-12-09 01:11:24 -0500

    complete user routes

--- routes/user.js
+++ routes/user.js
@@ -1,8 +1,54 @@
 const router = require('express').Router();
 const User = require('../models/User');
-const { verifyTokenAndAuth } = require('./veryifyToken');
+const { verifyTokenAndAuth, verifyTokenAndAdmin } = require('./veryifyToken');
 
+//GET
+router.get('/find/:id', verifyTokenAndAdmin, async (req, res) => {
+    try {
+        const user = await User.findById(req.params.id);
+        const { password, ...others } = user._doc;
+        res.status(200).json(others);
+    }catch(err) {
+        res.status(500).json(err);
+    }
+});
 
+//GET ALL USERS
+router.get('/', verifyTokenAndAdmin, async (req, res) => {
+    const query = req.query.new;
+    try {
+        const users = query ? await User.find().limit(5).sort({_id: -1}) : await User.find();
+        res.status(200).json(users);
+    }catch(err) {
+        res.status(500).json(err);
+    }
+});
+
+//GET USER STATS
+router.get("/stats", verifyTokenAndAdmin, async (req, res) => {
+    const date = new Date();
+    const lastYear = new Date(date.setFullYear(date.getFullYear() - 1));
+
+    try {
+        const data = await User.aggregate([
+            { $match: { createdAt: { $gte: lastYear } } },
+            {
+                $project: {
+                    month: { $month: "$createdAt" },
+                },
+            },
+            {
+                $group: {
+                    _id: "$month",
+                    total: { $sum: 1 }
+                }
+            }
+        ]);
+        res.status(200).json(data);
+    }catch(err) {
+        res.status(500).json(err);
+    }
+});
 
 //UPDATE
 router.put('/:id', verifyTokenAndAuth, async (req, res) => {
@@ -28,6 +74,7 @@ router.delete('/:id', verifyTokenAndAuth, async (req, res) => {
         res.status(500).json(err);
     }
 });
+
 
 module.exports = router;
 
