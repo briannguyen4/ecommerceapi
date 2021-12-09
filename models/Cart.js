ff17b2da8a4d3178f7cce5ce9d138e2fb4472202
briannguyen4 <brian4nguyen@gmail.com>
2021-12-08 20:57:26 -0500

    add cart and order models

--- models/Cart.js
+++ models/Cart.js
@@ -4,9 +4,11 @@ const CartSchema = new mongoose.Schema(
     {
         userId: { type: String, required: true} ,
         products: [
-            { productId: { type: String } },
-            { quantity: { type: Number, default: 1 } }
-        ],
+            {
+                productId: { type: String },
+                quantity: { type: Number, default: 1 },
+            }
+        ]
     },
     {
         timestamps: true
