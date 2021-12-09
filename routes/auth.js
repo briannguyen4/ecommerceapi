0062efd39184f55434434b26cebc12a0a2d2c97e
briannguyen4 <brian4nguyen@gmail.com>
2021-12-08 22:03:12 -0500

    add hashing function for password when user registers

--- routes/auth.js
+++ routes/auth.js
@@ -1,13 +1,14 @@
 const router = require('express').Router();
 const User = require('../models/User');
+const CryptoJS = require('crypto-js');
 
 //Register
 router.post('/register', async (req, res) => { // add async
     //Create model object
     const newUser = new User({
-        username: req.body.user,
+        username: req.body.username,
         email: req.body.email,
-        password: req.body.password,
+        password: CryptoJS.AES.encrypt(req.body.password, process.env.HASHING_KEY).toString(),
     });
     //Send to DB
     try {
