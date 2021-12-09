8bbd064d59db3d3a849cd53ba5256e87c7f2a315
briannguyen4 <brian4nguyen@gmail.com>
2021-12-08 20:49:52 -0500

    create product and cart models

--- models/User.js
+++ models/User.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 
-const userSchema = new mongoose.Schema(
+const UserSchema = new mongoose.Schema(
     {
         username: {type: String, required: true, unique: true},
         email: {type: String, required: true, unique: true},
@@ -12,4 +12,4 @@ const userSchema = new mongoose.Schema(
     }
 );
 
-module.exports = mongoose.model("User", userSchema);
+module.exports = mongoose.model("User", UserSchema); 
