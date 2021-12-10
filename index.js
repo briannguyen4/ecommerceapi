8eb10cdd84344ea282884351df3aa72aa29ea8d2
briannguyen4 <brian4nguyen@gmail.com>
2021-12-10 02:40:05 -0500

    complete order router

--- index.js
+++ index.js
@@ -5,6 +5,8 @@ const dotenv = require("dotenv");
 const userRoute = require("./routes/user");
 const authRoute = require("./routes/auth");
 const productRoute = require("./routes/product");
+const cartRoute = require("./routes/cart");
+const orderRoute = require("./routes/order");
 
 dotenv.config();
 
@@ -29,4 +31,9 @@ app.use('/api/users', userRoute);
 app.use('/api/auth', authRoute);
 
 app.use('/api/products', productRoute);
+
+app.use('/api/carts', cartRoute);
+
+app.use('/api/orders', orderRoute);
+
 
