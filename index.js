789adb0807c5ea3426b2c9ba2d7111407da9561f
briannguyen4 <brian4nguyen@gmail.com>
2021-12-10 03:20:48 -0500

    add stripe

--- index.js
+++ index.js
@@ -7,6 +7,8 @@ const authRoute = require("./routes/auth");
 const productRoute = require("./routes/product");
 const cartRoute = require("./routes/cart");
 const orderRoute = require("./routes/order");
+const stripeRoute = require("./routes/stripe");
+const cors = require('cors');
 
 dotenv.config();
 
@@ -24,6 +26,8 @@ mongoose.connect(
 // })
 
 // app.get("/api/test", () => console.log("Test is sucessful"));
+app.use(cors());
+
 app.use(express.json());
 
 app.use('/api/users', userRoute);
@@ -36,4 +40,4 @@ app.use('/api/carts', cartRoute);
 
 app.use('/api/orders', orderRoute);
 
-
+app.use('/api/checkout', stripeRoute);
